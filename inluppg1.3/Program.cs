11911d55f82a0b6a5db9ce5181580a06ad55b948
emsi06001 <emsi06001@E-PW0234N3>
2023-09-06 12:46:09 +0200

    Add project files.

---
+++ inluppg1.3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace inluppg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Varje nivå i pyramid har var sin rad. Jag radbryter med \n
+            Console.WriteLine("" +
+                "   /\\\n" +
+                "  /  \\\n" +
+                " /    \\\n" +
+                "/      \\\n");
+        }
+    }
+}
